c6c5e2d36a64eba76368c8d04312beb2ad0eec7e
BlueLancaster <a1085521@gmail.com>
2022-01-02 02:10:24 +0800

    db建置完成

---
+++ MeetingManager/models.py
@@ -0,0 +1,137 @@
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import backref
+
+from MeetingManager import db
+
+
+class Meeting(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    type = db.Column(db.String(10), nullable=False)
+    datetime = db.Column(db.String(30), nullable=False)
+    place = db.Column(db.String(30), nullable=False)
+    chairman = db.Column(db.Integer, db.ForeignKey('member.id'))
+    minuteTaker = db.Column(db.Integer, db.ForeignKey('member.id'))
+    welcomeSpeech = db.Column(db.Text)
+    announce = db.relationship('Announce', backref='meeting', lazy=True, cascade='all, delete-orphan')
+    appendix = db.relationship('Appendix', backref='meeting', lazy=True, cascade='all, delete-orphan')
+    discussion = db.relationship('Discussion', backref='meeting', lazy=True, cascade='all, delete-orphan')
+    extempore = db.relationship('Extempore', backref='meeting', lazy=True, cascade='all, delete-orphan')
+    attend = association_proxy('attendanceAssociation', 'member', creator=lambda member: Attend(member=member))
+
+    def __init__(self, name, type, datetime, place):
+        self.name = name
+        self.type = type
+        self.datetime = datetime
+        self.place = place
+
+
+'''
+meeting=Meeting(name='mouse',type=4,datetime='4/4',place='44')
+member=Member(name='a',sex='girl',phone='4234',email='a4122@yahoo.com',identity=4,password='4645')
+'''
+
+
+class Announce(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    meetingId = db.Column(db.Integer, db.ForeignKey('meeting.id'))
+
+    def __init__(self, content):
+        self.content = content
+
+
+class Discussion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    brief = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    result = db.Column(db.Text)
+    meetingId = db.Column(db.Integer, db.ForeignKey('meeting.id'))
+
+    def __init__(self, brief, content):
+        self.brief = brief
+        self.content = content
+
+
+class Extempore(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    brief = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    result = db.Column(db.Text)
+    meetingId = db.Column(db.Integer, db.ForeignKey('meeting.id'))
+
+    def __init__(self, brief, content):
+        self.brief = brief
+        self.content = content
+
+
+class Appendix(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filePath = db.Column(db.String(30), nullable=False)
+    meetingId = db.Column(db.Integer, db.ForeignKey('meeting.id'))
+
+
+class Member(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    sex = db.Column(db.String(6), nullable=False)
+    phone = db.Column(db.String(15), nullable=False)
+    email = db.Column(db.String(60), nullable=False)
+    identity = db.Column(db.String(10), nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    assistant = db.relationship('Assistant', backref="personalData", uselist=False, cascade='all, delete-orphan')
+    expert = db.relationship('Expert', backref="personalData", uselist=False, cascade='all, delete-orphan')
+    student = db.relationship('Student', backref="personalData", uselist=False, cascade='all, delete-orphan')
+    externalTeacher = db.relationship('ExternalTeacher', backref="personalData", uselist=False,
+                                      cascade='all, delete-orphan')
+    intramuralTeacher = db.relationship('IntramuralTeacher', backref="personalData", uselist=False,
+                                        cascade='all, delete-orphan')
+    attend = association_proxy('attendanceAssociation', 'meeting', creator=lambda meeting: Attend(meeting=meeting))
+
+    def __repr__(self):
+        return f"Member('{self.id}','{self.name}','{self.sex}',)"
+
+
+class Attend(db.Model):
+    meetingId = db.Column(db.Integer, db.ForeignKey('meeting.id'), primary_key=True)
+    memberId = db.Column(db.Integer, db.ForeignKey('member.id'), primary_key=True)
+    attendOrNot = db.Column(db.Boolean, default=False)
+    meeting = db.relationship(Meeting, backref=backref('attendanceAssociation', cascade='all, delete-orphan'))
+    member = db.relationship(Member, backref=backref('attendanceAssociation', cascade='all, delete-orphan'))
+
+
+class Assistant(db.Model):
+    id = db.Column(db.Integer, db.ForeignKey('member.id'), primary_key=True)
+    officePhone = db.Column(db.String(15))
+
+
+class Expert(db.Model):
+    id = db.Column(db.Integer, db.ForeignKey('member.id'), primary_key=True)
+    companyName = db.Column(db.String(40), nullable=True)
+    title = db.Column(db.String(20), nullable=True)
+    officePhone = db.Column(db.String(15))
+    address = db.Column(db.String(60))
+    bankAccount = db.Column(db.String(30))
+
+
+class Student(db.Model):
+    id = db.Column(db.Integer, db.ForeignKey('member.id'), primary_key=True)
+    studentId = db.Column(db.String(10), nullable=False, unique=True)
+    eSystem = db.Column(db.String(10), nullable=False)
+    grade = db.Column(db.Integer, nullable=False)
+
+
+class ExternalTeacher(db.Model):
+    id = db.Column(db.Integer, db.ForeignKey('member.id'), primary_key=True)
+    school = db.Column(db.String(30), nullable=False)
+    department = db.Column(db.String(30), nullable=False)
+    title = db.Column(db.String(20), nullable=True)
+    officePhone = db.Column(db.String(15))
+    address = db.Column(db.String(60))
+    bankAccount = db.Column(db.String(30))
+
+
+class IntramuralTeacher(db.Model):
+    id = db.Column(db.Integer, db.ForeignKey('member.id'), primary_key=True)
+    rank = db.Column(db.Integer, nullable=False)
+    officePhone = db.Column(db.String(15))
--- MeetingManager/routes.py
+++ MeetingManager/routes.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, url_for, jsonify
-from MeetingManager import app
+from flask import Flask, render_template, url_for, jsonify, request
+from MeetingManager import app, db
+from MeetingManager.models import *
 
 
 @app.route("/")
 @app.route("/home")
 def home():
+    db.create_all()
     return render_template("home.html")
 
 
@@ -33,11 +35,80 @@ def login():
     return render_template('login.html')
 
 
+# ajax請求會議資料
 @app.route("/getMeetingMinutes")
 def getMeetingMinutes():
     return jsonify({'name': '高雄大學第987次會議', 'date': '2021/01/01', 'place': '工學院廁所'})
 
 
+# ajax請求人員資料
 @app.route("/getMemberData")
 def getMemberData():
     return jsonify({'name': '黑奴', 'sex': '男', 'phone': '09755111'})
+
+
+# 接收會議紀錄表單
+@app.route("/submitMeetingMinutes", methods=['POST'])
+def submitMeetingMinutes():
+    #   discussionNameList = request.form.getlist('discussionName')
+    #   print(request.values.get('name'))
+    discussionBriefList = request.form.getlist('discussionName')
+    discussionContentList = request.form.getlist('discussionContent')
+    discussionResultList = request.form.getlist('discussionResult')
+    announceContentList = request.form.getlist('announceContent')
+    extemporeBriefList = request.form.getlist('extemporeName')
+    extemporeContentList = request.form.getlist('extemporeContent')
+    extemporeResultList = request.form.getlist('extemporeResult')
+    # print(request.values.get('name'))
+    # print(request.values.get('date'))
+    # print(request.values.get('place'))
+    # print(request.values.get('type'))
+    # 系務會議 系教評會 系課程委員會 招生暨學生事務委員會 系發展委員會
+    data = request.values
+    temp = data.get('type')
+    meetingType = '系務會議'
+    if temp == 0:
+        meetingType = '系務會議'
+    elif temp == 1:
+        meetingType = '系教評會'
+    elif temp == 2:
+        meetingType = '系課程委員會'
+    elif temp == 3:
+        meetingType = '招生暨學生事務委員會'
+    elif temp == 4:
+        meetingType = '系發展委員會'
+
+    newMeeting = Meeting(data.get('name'), meetingType, data.get('date'), data.get('place'))
+
+    if discussionBriefList:
+        for i in range(len(discussionBriefList)):
+            discussion = Discussion(discussionBriefList[i], discussionContentList[i])
+            newMeeting.discussion.append(discussion)
+            db.session.add(discussion)
+    if announceContentList:
+        for i in range(len(announceContentList)):
+            announce = Announce(announceContentList[i])
+            newMeeting.announce.append(announce)
+            db.session.add(announce)
+    if extemporeBriefList:
+        for i in range(len(extemporeBriefList)):
+            extempore = Extempore(extemporeBriefList[i], extemporeContentList[i])
+            newMeeting.extempore.append(extempore)
+            db.session.add(extempore)
+
+    db.session.add(newMeeting)
+    db.session.commit()
+
+    return render_template("meeting_manage.html")
+
+
+# 接收人員資料表單
+@app.route("/submitMemberData", methods=['POST'])
+def submitMemberData():
+    print(request.values.get('name'))
+    print(request.values.get('role'))
+    print(request.values.get('sex'))
+    print(request.values.get('phone'))
+    print(request.values.get('email'))
+
+    return render_template("member_manage.html")
